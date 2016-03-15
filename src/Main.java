6d2c63c371e3702502e46f16eec7ce9f6fa9a356
euglav112 <euglav112@gmail.com>
2016-03-15 10:35:25 +0300

    Back to start

--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,5 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        System.out.println(345);
-        System.out.println("Hello Underworld!");
-        int a = 10;
-        for (int i = 0; i < 50; i++) {
-            i--;
-            System.out.println("Text");
-        }
-
-        Wrong code;
     }
 }
