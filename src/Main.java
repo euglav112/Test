92b5601674f83d99f85d79714d02d9eb83bcc61d
euglav112 <euglav112@gmail.com>
2016-03-15 11:10:59 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         // ужасный и бесполезный код, никто так не пишет
+        // еще пара бесполезных комментов
     }
 }
