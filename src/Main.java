6158a640848565b8c2b9b9e1ff57fa92ae9e5d59
euglav112 <euglav112@gmail.com>
2016-03-15 11:31:55 +0300

    KJEGbMS<FHN D<FHz,g

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,5 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         // ужасный и бесполезный код, никто так не пишет
-    }
+        System.out.println("  dfghsrejzgjshksuksdul  ");
 }
