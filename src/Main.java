8fc6370420f7fb0778f4ab3c96296887172a9ca7
euglav112 <euglav112@gmail.com>
2016-03-17 09:17:45 +0300

    Delete useless code.

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        // ужасный и бесполезный код, никто так не пишет
-        System.out.println("  dfghsrejzgjshksuksdul  ");
-        int a = 4 + 8;
-        //comment
+
     }
 }
